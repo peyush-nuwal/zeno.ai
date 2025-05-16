c03d4c8c7dc728dda786e0c9278f5c377702ef11
peyush <piyushnawal19.com>
2025-05-16 15:07:47 +0530

    updated api url in frontend

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const URL="http://localhost:3000"
+const URL ="https://zeno-ai-d9eq.onrender.com"
 const reviewCode= async(code)=>{
     const response= await axios.post(`${URL}/ai/get-review`,
        { code}
